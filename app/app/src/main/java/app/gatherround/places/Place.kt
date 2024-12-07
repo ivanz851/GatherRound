9306c063bbb62b5e8b66e39c49419a7269ec7a2d
ivanz-thinkpad <Ivan.zakharov.ru@yandex.ru>
2024-12-07 08:28:03 +0300

    fix parsing of nearest subway names

--- app/app/src/main/java/app/gatherround/places/Place.kt
+++ app/app/src/main/java/app/gatherround/places/Place.kt
@@ -1,23 +1,39 @@
 package app.gatherround.places
 
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 
 @Serializable
 data class Place(
     val id: Int,
     val title: String,
     val address: String,
-    val location: String,
-    val coords: Coords? = null,
-    val phone: String? = null,
-    val description: String? = null,
-    val categories: List<String> = emptyList(),
-    val rating: Double? = null,
-    val imageUrl: String? = null
+    val coords: Coordinates? = null,
+    @Serializable(with = SubwayDeserializer::class)
+    val subway: List<String> = emptyList(),
 ) {
     @Serializable
-    data class Coords(
+    data class Coordinates(
         val lat: Double?,
         val lon: Double?
     )
+
+    object SubwayDeserializer : KSerializer<List<String>> {
+        override val descriptor: SerialDescriptor =
+            PrimitiveSerialDescriptor("Subway", PrimitiveKind.STRING)
+
+        override fun serialize(encoder: Encoder, value: List<String>) {
+            encoder.encodeString(value.joinToString(", "))
+        }
+
+        override fun deserialize(decoder: Decoder): List<String> {
+            val subwayString = decoder.decodeString()
+            return subwayString.split(",").map { it.trim() }
+        }
+    }
 }
