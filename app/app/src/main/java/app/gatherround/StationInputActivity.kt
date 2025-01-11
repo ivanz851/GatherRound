ed2c448251fd0abb5c663d7fee548ab42dd2d305
ivanz-thinkpad <Ivan.zakharov.ru@yandex.ru>
2025-01-11 16:22:07 +0300

    implement removing stations from the list

--- app/app/src/main/java/app/gatherround/StationInputActivity.kt
+++ app/app/src/main/java/app/gatherround/StationInputActivity.kt
@@ -15,18 +15,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import app.gatherround.metro.MetroData
 import app.gatherround.metro.MetroGraph
-import app.gatherround.metro.Station
-import app.gatherround.places.Place
 import app.gatherround.places.PlacesData
 import kotlinx.coroutines.*
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 
 class StationInputActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -78,17 +75,28 @@ fun StationInputScreen(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         stations.forEachIndexed { index, station ->
-            StationInputField(
-                index = index,
-                station = station,
-                stationsNames = stationsNames,
-                onValueChange = { newStation, ind ->
-                    stations = stations.toMutableList().apply { set(ind, newStation) }
-                },
-                onSelect = { station, line ->
-                    selectedStations.value.add(station to line)
-                }
-            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                StationInputField(
+                    index = index,
+                    station = station,
+                    stationsNames = stationsNames,
+                    onValueChange = { newStation, ind ->
+                        stations = stations.toMutableList().apply { set(ind, newStation) }
+                    },
+                    onSelect = { station, line ->
+                        selectedStations.value.add(station to line)
+                    },
+                    onClose = { ind ->
+                        val stationToRemove = stations[ind]
+                        stations = stations.toMutableList().apply { removeAt(ind) }
+                        selectedStations.value.removeIf { it.first == stationToRemove }
+                    }
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+            }
         }
 
         if (stations.size < 6) {
@@ -119,15 +127,12 @@ fun StationInputScreen(
                             findOptimalPlaces(graph, chosenStations, placesData)
                         }!!
 
-                        //val eventsJson = Json.encodeToString(events)
-
                         val intent = Intent(context, EventListActivity::class.java).apply {
                             putExtra("places_json", eventsJson)
                         }
                         context.startActivity(intent)
                     } catch (e: Exception) {
                         e.printStackTrace()
-                        // Показываем сообщение об ошибке или обрабатываем исключение
                     }
                 }
             },
@@ -144,57 +149,84 @@ fun StationInputScreen(
     }
 }
 
+
 @Composable
 fun StationInputField(
     index: Int,
     station: String,
     stationsNames: List<Pair<String, String>>,
     onValueChange: (String, Int) -> Unit,
-    onSelect: (String, String) -> Unit
+    onSelect: (String, String) -> Unit,
+    onClose: (Int) -> Unit
 ) {
-    var text by remember { mutableStateOf(station) }
+    var text by remember { mutableStateOf(station) } // Состояние для текущей станции
     var expanded by remember { mutableStateOf(false) }
     var filteredStations by remember { mutableStateOf(stationsNames) }
 
     Box(modifier = Modifier.fillMaxWidth()) {
         Column(modifier = Modifier.fillMaxWidth()) {
-            BasicTextField(
-                value = text,
-                onValueChange = { newText: String ->
-                    text = newText
-                    filteredStations =
-                        stationsNames.filter { it.first.startsWith(newText, ignoreCase = true) }
-                    expanded = true
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp)
-                    .border(1.dp, Color.Gray),
-                textStyle = TextStyle(color = Color.Black)
-            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                BasicTextField(
+                    value = text,
+                    onValueChange = { newText: String ->
+                        text = newText
+                        filteredStations =
+                            stationsNames.filter { it.first.startsWith(newText, ignoreCase = true) }
+                        expanded = true
+                        onValueChange(newText, index) // Обновляем состояние в списке
+                    },
+                    modifier = Modifier
+                        .weight(1f) // Пропорциональное заполнение пространства
+                        .padding(8.dp)
+                        .border(1.dp, Color.Gray),
+                    textStyle = TextStyle(color = Color.Black)
+                )
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                IconButton(
+                    onClick = {
+                        onClose(index) // Удаляем станцию по индексу
+                    },
+                    modifier = Modifier.size(48.dp) // Увеличиваем область клика
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Close,
+                        contentDescription = "Удалить станцию",
+                        tint = Color.Black,
+                    )
+                }
+            }
 
             if (expanded && filteredStations.isNotEmpty()) {
-                LazyColumn(modifier = Modifier.fillMaxWidth()) {
-                    items(filteredStations) { (station, line) ->
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .heightIn(max = 120.dp) // Ограничение высоты списка
+                ) {
+                    items(filteredStations) { (stationName, lineName) ->
                         Row(
                             modifier = Modifier
                                 .fillMaxWidth()
                                 .clickable {
-                                    text = station
-                                    onValueChange(station, index)
-                                    onSelect(station, line)
+                                    text = stationName
+                                    onValueChange(stationName, index)
+                                    onSelect(stationName, lineName)
                                     expanded = false
                                 }
                                 .padding(8.dp),
                             horizontalArrangement = Arrangement.SpaceBetween
                         ) {
                             Text(
-                                text = station,
+                                text = stationName,
                                 modifier = Modifier.weight(1f),
                                 color = Color.Black
                             )
                             Text(
-                                text = line,
+                                text = lineName,
                                 color = Color.Gray
                             )
                         }
